871df02989418bb0979ceb53812b9f27eda19c79
Raghudeepbathula <Raghudeep.bathula98@gmail.com>
2018-12-14 11:06:53 +0100

    Update Room.java

--- src/Room.java
+++ src/Room.java
@@ -2,17 +2,48 @@ public class Room {
 
     private int roomNumber;
     private int numberOfBeds;
-    private boolean hasBalcony;
     private double pricePerNight;
     private boolean isBooked;
 
-
-
-    public void Room(int roomNbr, int numberOfBeds, boolean hasBalcony, double pricePerNight) {
-
+    public Room(int roomNumber, int numberOfBeds, double pricePerNight, boolean isBooked) {
+        this.roomNumber = roomNumber;
+        this.numberOfBeds = numberOfBeds;
+        this.pricePerNight = pricePerNight;
+        this.isBooked = isBooked;
     }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public boolean isIsBooked() {
+        return isBooked;
+    }
+
+    public void setIsBooked(boolean isBooked) {
+        this.isBooked = isBooked;
+    }
+
 }
-
-
 
 
