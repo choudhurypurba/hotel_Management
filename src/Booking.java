561007f1e76167adf5775435f8710611ea64bc2f
mustafahkr <45200515+mustafahkr@users.noreply.github.com>
2018-12-14 11:13:12 +0100

    Update Booking.java

--- src/Booking.java
+++ src/Booking.java
@@ -1,25 +1,64 @@
-
 import java.util.Date;
 
 import java.util.List;
 
 public class Booking {
 
-
-
     private int bookingId;
     private Date checkInDate;
     private Date checkOutDate;
     private double totalPrice;
+    private List<Room> roomList;
 
+    public void Booking(int id, Date checkindate, Date checkoutdate, List<Room> rooms) {
 
-
-    public void Booking(int id, List<Room> rooms)
-    {
+        this.bookingId = id;
+        this.checkInDate = checkindate;
+        this.checkOutDate = checkoutdate;
+        this.roomList = rooms;
+        for (int i = 0; i < rooms.size(); i++) {
+            this.totalPrice = this.totalPrice + rooms.get(i).getPricePerNight();
+        }
 
     }
 
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public List<Room> getRoomList() {
+        return roomList;
+    }
 
 }
+
 
 
